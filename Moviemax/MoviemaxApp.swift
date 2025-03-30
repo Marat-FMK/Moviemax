4ce8bae231eb2a8c3823a30686a76b76f5e8010e
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-03-30 20:55:07 +0300

    Initial Commit

---
+++ Moviemax/MoviemaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviemaxApp.swift
+//  Moviemax
+//
+//  Created by Marat Fakhrizhanov on 30.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviemaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
