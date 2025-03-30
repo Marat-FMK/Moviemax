5ef88c890bbfa24fbb35f807945c9ce2e75a764f
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-03-31 02:49:02 +0300

    add secure tf

--- Moviemax/LoginView/CustomSecretPasswordTFView.swift
+++ Moviemax/LoginView/CustomSecretPasswordTFView.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct CustomSecretPasswordTFView: View {
+    @Binding var visible: Bool
+    @Binding var answer: String
+    let title: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack( alignment: .leading) {
+            Text(title)
+                .foregroundStyle(.gray)
+                .font(.system(size: 14))
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 24)
+                    .frame(height: 56)
+                    .foregroundStyle(.tf)
+                if visible {
+                    HStack {
+                        TextField("Enter your password", text: $answer)
+                        
+                        Button{
+                            visible.toggle()
+                        } label: {
+                            Image(systemName: "eye.slash.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 24)
+                                .foregroundStyle(.gray)
+                        }
+                        .buttonStyle(.plain)
+                    }
+                    .padding(.horizontal, 15)
+                } else {
+                    HStack{
+                        SecureField("Enter your password", text: $answer)
+                        
+                        Button{
+                            visible.toggle()
+                        } label: {
+                            Image(systemName: "eye.slash.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 24)
+                                .foregroundStyle(.gray)
+                        }
+                        .buttonStyle(.plain)
+                    }
+                    .padding(.horizontal, 15)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    CustomSecretPasswordTFView()
+    CustomSecretPasswordTFView(visible: .constant(false), answer: .constant("12345"), title: "Password")
 }
