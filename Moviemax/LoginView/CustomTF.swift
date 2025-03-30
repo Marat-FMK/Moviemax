e09b05af82fa7c954f602bad48ce9ee70f14742b
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-03-31 01:09:23 +0300

    add element

---
+++ Moviemax/LoginView/CustomTF.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTF.swift
+//  Moviemax
+//
+//  Created by Marat Fakhrizhanov on 30.03.2025.
+//
+
+import SwiftUI
+
+struct CustomTF: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTF()
+}
---
+++ Moviemax/LoginView/LongPurpleButton.swift
@@ -0,0 +1,18 @@
+//
+//  LongPurpleButton.swift
+//  Moviemax
+//
+//  Created by Marat Fakhrizhanov on 31.03.2025.
+//
+
+import SwiftUI
+
+struct LongPurpleButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LongPurpleButton()
+}
