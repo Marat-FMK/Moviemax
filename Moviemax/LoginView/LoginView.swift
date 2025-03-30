b8b7524ad8320383752888f00a472f2969035349
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-03-31 01:10:03 +0300

    add loginView

--- Moviemax/LoginView/LoginView.swift
+++ Moviemax/LoginView/LoginView.swift
@@ -8,8 +8,70 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @StateObject var viewModel = LoginViewViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                
+                Text("Login")
+                    .font(.system(size: 24))
+                    .bold()
+                
+                Spacer()
+                
+                VStack (alignment: . leading, spacing: 10) {
+                    CustomTF(answer: $viewModel.userEmail, title: "Email", tfBGtext: "Enter your address")
+                    CustomTF(answer: $viewModel.userPassword, title: "Password", tfBGtext: "Enter your password")
+                    
+                    RememberHStackView(value: $viewModel.rememberMe)
+                    .padding(.top, 20)
+                    
+                    //BUTTONs
+                    VStack(spacing: 16) {
+                        LongPurpleButton(title: "Sign In", action: viewModel.signIn)
+                    //divider
+                        HStack {
+                            Rectangle()
+                                .frame(height: 2)
+                                .foregroundStyle(.divider)
+                            
+                            Text("Or continue with")
+                                .frame(width: 150)
+                                .foregroundStyle(.divider)
+                            
+                            Rectangle()
+                                .frame(height: 2)
+                                .foregroundStyle(.divider)
+                        }
+                        .padding(.horizontal,30)
+                        
+                        
+                    //google button
+                       
+                    }
+                    .padding(.top, 50)
+                }
+                
+                Spacer()
+                
+                //SIGN UP text
+                HStack {
+                    Text("Don't have an account?")
+                    
+                    NavigationLink {
+                        SignUpView()
+                    } label: {
+                        Text("Sign up")
+                            .foregroundStyle(.forgot)
+                    }
+                }
+                .padding(.bottom, 20)
+                
+                
+            }
+            .padding(.horizontal, 24)
+        }
     }
 }
 
