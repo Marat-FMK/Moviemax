401d9c9ddeb43ff25e71dee7f0a33fc0a347a7f6
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-04-01 19:46:33 +0300

    add Search Field

--- Moviemax/SearchView/SearchField.swift
+++ Moviemax/SearchView/SearchField.swift
@@ -8,11 +8,62 @@
 import SwiftUI
 
 struct SearchField: View {
+    @Binding var searchText: String
+    @Binding var presentFilter: Bool
+    let action: () -> Void // start search
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        ZStack{
+            RoundedRectangle(cornerRadius: 24).stroke(style: StrokeStyle(lineWidth: 2))
+                .foregroundStyle(.buttonPurple)
+                .frame(height: 52)
+            HStack{
+                Button{
+                    action()
+                } label: {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 18)
+                }
+                .buttonStyle(.plain)
+                
+                TextField("SearchText", text: $searchText)
+                    .onSubmit {
+                        action()
+                    }
+                
+                Button{
+                    searchText = ""
+                } label: {
+                    Image(systemName: "xmark")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 18)
+                }
+                .buttonStyle(.plain)
+                
+                Divider()
+                    .frame(height: 18)
+                
+                Button{
+                    presentFilter.toggle()
+                } label: {
+                    Image(systemName: "slider.horizontal.3")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 18)
+                }
+                .buttonStyle(.plain)
+            }
+            .padding(.horizontal,15)
+        }
+        
     }
 }
 
 #Preview {
-    SearchField()
+    SearchField(searchText: .constant("Drifting"), presentFilter: .constant(false), action: { print("start search preview")})
 }
