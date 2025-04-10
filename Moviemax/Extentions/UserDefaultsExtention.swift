8b8d631827c69b697ff2213d7807f06cccb64bad
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-04-11 00:32:09 +0300

    fix re-creating the file

--- Moviemax/Extentions/UserDefaultsExtention.swift
+++ Moviemax/Extentions/UserDefaultsExtention.swift
@@ -5,32 +5,32 @@
 //  Created by Julia Martcenko on 07/04/2025.
 //
 
-import Foundation
-
-extension UserDefaults {
-	private var favoriteKey: String { "favoriteFilmIDs" }
-
-	var favoriteFilmIDs: Set<String> {
-		get {
-			let array = array(forKey: favoriteKey) as? [String] ?? []
-			return Set(array)
-		}
-		set {
-			set(Array(newValue), forKey: favoriteKey)
-		}
-	}
-
-	func isFavorite(filmID: String) -> Bool {
-		favoriteFilmIDs.contains(filmID)
-	}
-
-	func toggleFavorite(filmID: String) {
-		var current = favoriteFilmIDs
-		if current.contains(filmID) {
-			current.remove(filmID)
-		} else {
-			current.insert(filmID)
-		}
-		favoriteFilmIDs = current
-	}
-}
+//import Foundation
+//
+//extension UserDefaults {
+//	private var favoriteKey: String { "favoriteFilmIDs" }
+//
+//	var favoriteFilmIDs: Set<String> {
+//		get {
+//			let array = array(forKey: favoriteKey) as? [String] ?? []
+//			return Set(array)
+//		}
+//		set {
+//			set(Array(newValue), forKey: favoriteKey)
+//		}
+//	}
+//
+//	func isFavorite(filmID: String) -> Bool {
+//		favoriteFilmIDs.contains(filmID)
+//	}
+//
+//	func toggleFavorite(filmID: String) {
+//		var current = favoriteFilmIDs
+//		if current.contains(filmID) {
+//			current.remove(filmID)
+//		} else {
+//			current.insert(filmID)
+//		}
+//		favoriteFilmIDs = current
+//	}
+//}
