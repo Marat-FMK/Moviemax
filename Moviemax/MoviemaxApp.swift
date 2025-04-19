7d200a19e617639ffc0a4685ac7328efb6a404d6
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-04-20 00:38:32 +0300

    add logic for start app View

--- Moviemax/MoviemaxApp.swift
+++ Moviemax/MoviemaxApp.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import FirebaseCore
+import Firebase
+import FirebaseAuth
 
 class AppDelegate: NSObject, UIApplicationDelegate { //firebase
   func application(_ application: UIApplication,
@@ -21,9 +23,15 @@ class AppDelegate: NSObject, UIApplicationDelegate { //firebase
 struct MoviemaxApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate // firebase
     
+    var authComplete = UserDefaults.standard.bool(forKey: "authComplete")
+    
     var body: some Scene {
         WindowGroup {
-            TabBarView()
+            if authComplete {
+               TabBarView()
+            } else {
+               LoginView()
+            }
         }
     }
 }
