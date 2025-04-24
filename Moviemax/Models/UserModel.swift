f5ef1ab320c0c40a38217bba4c3837a0f06f0578
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-04-24 23:26:47 +0300

    comment unused model

--- Moviemax/Models/UserModel.swift
+++ Moviemax/Models/UserModel.swift
@@ -4,17 +4,17 @@
 //
 //  Created by Julia Martcenko on 14/04/2025.
 //
-
-import Foundation
-
-struct User: Identifiable {
-	let id: String
-	var firstName: String
-	var lastName: String
-	let password: String
-	var email: String
-	let dateOfBirth: String
-	let gender: String
-	let location: String
-	let login: String
-}
+//
+//import Foundation
+//
+//struct User: Identifiable {
+//	let id: String
+//	var firstName: String
+//	var lastName: String
+//	let password: String
+//	var email: String
+//	let dateOfBirth: String
+//	let gender: String
+//	let location: String
+//	let login: String
+//}
