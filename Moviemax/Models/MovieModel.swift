ee3e201e4abc3383e4e6b2e36cd699918451234f
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-04-29 22:17:17 +0300

    add new model for MOvies

--- Moviemax/Models/MovieModel.swift
+++ Moviemax/Models/MovieModel.swift
@@ -21,8 +21,38 @@ struct Movie: Identifiable, Equatable {
 	let castCrew: [CrewMemberModel]
 }
 
-struct CrewMemberModel: Hashable, Equatable {
+struct CrewMemberModel: Hashable, Equatable{
 	let image: String
 	let name: String
 	let role: String
+}
+
+
+// API
+
+struct Movies: Codable {
+    let docs: [ApiMovie]
+}
+
+struct ApiMovie: Identifiable, Codable {
+    let id: Int?
+    let name: String?
+    let alternativeName: String?
+    let year: Int?
+    let description: String?
+    let genres: [Genre]?
+    let movieLenght: Int?
+    let rating: Rating?
+    let poster: Poster?
+}
+
+struct Genre: Codable {
+    let name: String?
+}
+
+struct Rating: Codable {
+    let imdb: Double?
+}
+struct Poster: Codable {
+    let url: String?
 }
