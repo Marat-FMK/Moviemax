cd1ea2a6fe04b8c88ee57df8d9c48284c28ea4af
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-05-08 16:27:13 +0300

    clear testing methon

--- Moviemax/Views/LoginView/LoginView.swift
+++ Moviemax/Views/LoginView/LoginView.swift
@@ -103,12 +103,6 @@ struct LoginView: View {
                 }
                 .padding(.bottom, 20)
             }
-            .onAppear {
-                ApiService().fetchFilmInfo(id: 2213) { movie in
-                    print(movie)
-                    print("---->>>>> detail moVIE")
-                }
-            }
             .alert("Auth error", isPresented: $presentAlert, actions: {}, message: {
                 Text("Please, check your password or email")
             })
